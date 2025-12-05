08d2b7f6d00f129854a2f636eb2cc2d7de33987c
michelleguo87 <23hdqq@queensu.ca>
2025-12-04 19:22:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,23 +9,53 @@ def binarySearch(nums, target):
    upper = len(nums)-1
    lower = 0 
    while upper > lower:  
-      mid = (lower + upper)//2 
-      if nums[mid] == target: 
-         return mid
-      elif nums[mid] < target: 
-         lower = mid + 1 
-      elif nums[mid] > target: 
-         upper = mid - 1
-   return  
+      mid = (lower + upper)//2
+      iteration += 1 
+      #show left, right, middle pointers as text each iteration 
+      pointers = ""
+      pointers += "<p>Iteration: " + str(iteration) + "</p>" #AI Disclaimer: asked how html works in gradio and for examples on how to implement colors
+      pointers += "<p style='color:orange;'>left bound index: " + str(left) + "</p>" 
+      pointers += "<p style='color:green;'>middle index: " + str(mid) + "</p>"
+      pointers += "<p style='color:blue;'>right bound index: " + str(right) + "</p>"
+
+      yield pointers 
+      if array[mid] == target:
+         yield "<p style='color:green;'>Target number found at index: " + str(mid) + "</p>"
+         return 
+      elif array[mid] < target:
+         left = mid + 1
+      elif array[mid] > target: 
+         right = mid - 1 
+    yield "<p style='color:red;'>Target number not found</p>"
+    return 
 
 with gr.Blocks() as demo: 
    title_text = gr.HTML("<p style= 'font-size: 20px;'>Binary Search Visualization</p>")
 
     #initial visuals
-    array = gr.Textbox(value = "1, 2, 5, 7, 8, 10, 12, 15, 16", label = "Array") 
+    array_input = gr.Textbox(label = "Please enter an array of integers that has at least one number and less than 1000 numbers, seperated by commas. Eg: 1,2,3")  
     target_input = gr.Number(label = "Please Enter the Target Number") 
 
     #buttons 
     startButton = gr.Button("Start Search")
     resetButton = gr.Button("Reset Search")
 
+    def run(array, target):
+       array_list = []
+       for char in array.split(","): #AI Disclaimer: asked prompt: how to convert string into list of integers while ignoring commas.
+          array_list.append(char) 
+        #display different steps and colored array each iteration
+       steps = ""
+       for step in binarySearch(target, array_list): 
+          steps += step
+          time.sleep(2) #Learned from gradio streaming output document example 
+          yield steps
+
+    #reset the search 
+    def reset():
+       return "", "" 
+
+   startButton.click(run, inputs = [array_input, target_input], outputs = [output_steps])
+   resetButton.click(reset, outputs = [colored_array, output_steps]) 
+
+demo.launch() 
